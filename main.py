a03ea68f313e359bc59a8557fbd92e92f2dadb8c
brakcetin <brakcetin660@gmail.com>
2025-01-16 10:51:08 +0300

    first GET operation

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ from fastapi import FastAPI
 
 app = FastAPI()  # created an object from FastAPI class
 
-
 # is an endpoint that we say after each /. for example blabla.com/showcourses
 # showcourses is an endpoint
+
+courses_db = [  # create a manuel temporary database
+    {'id': 1, 'instructor': 'Burak', 'title': 'Python', 'category': 'Development'},
+    {'id': 2, 'instructor': 'Buket', 'title': 'Java', 'category': 'Development'},
+    {'id': 3, 'instructor': 'Mahmut', 'title': 'Jenkins', 'category': 'DevOps'},
+    {'id': 4, 'instructor': 'Burak', 'title': 'Kubernetes', 'category': 'DevOps'},
+    {'id': 5, 'instructor': 'Cem', 'title': 'Machine Learning', 'category': 'AI'},
+    {'id': 6, 'instructor': 'Lale', 'title': 'Deep Learning', 'category': 'AI'},
+]
+
 
 @app.get("/hello")  # if you write "/hello", in 127.0.0.1:8000 you will see Not Found! message
 # but in 127.0.0.1.800/hello, you will see {"message": "Hello World"} message
 async def hello_world():  # we need to work asynchronous with web application
     # but via FastAPI, without "async" word, it will run without any problem
     return {"message": "Hello World"}  # dictionary => key, value => good for JSON format
+
+
+# JSON -> JavaScript Object Notation
 
 # at console, run: uvicorn main:app --reload
 # uvicorn: run the server via unicorn
@@ -31,3 +43,10 @@ POST:   The data sent to the server with POST is stored in the request body of t
         better and reliable for bigger datas
         send to the server
 '''
+
+@app.get("/courses")  # if you write "/hello", in 127.0.0.1:8000 you will see Not Found! message
+# but in 127.0.0.1.800/hello, you will see {"message": "Hello World"} message
+async def hello_world():  # we need to work asynchronous with web application
+    # but via FastAPI, without "async" word, it will run without any problem
+    return courses_db  # dictionary => key, value => good for JSON format
+
