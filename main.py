b97459035f67e60eb821080333e9a2beea09b8a0
brakcetin <brakcetin660@gmail.com>
2025-01-23 15:46:44 +0300

    Query, path&query, post, update and delete

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()  # created an object from FastAPI class
 
@@ -60,10 +60,12 @@ async def get_course(course_title: str):
     for course in courses_db:
         if course.get('title').casefold() == course_title.casefold():  # casefold(): handling case sensitivity
             return course
+
+
 # 127.0.0.1:800/courses/java = working
 # 127.0.0.1:800/courses/scala = null
 
-# this function is not using
+# this function is not used
 @app.get("/courses/{course_id}")  # creating dynamic titles
 async def get_course_by_id(course_id: str):  # functions' names can be same
     for course in courses_db:
@@ -83,7 +85,90 @@ async def get_course_by_id(course_id: int):  # functions' names can be same
     for course in courses_db:
         if course.get('id') == course_id:  # you cannot use casefold() for int
             return course
+
+
 # 127.0.0.1:800/courses/1 = working
 # 127.0.0.1:800/courses/3 = working
 # 127.0.0.1:800/courses/java = null
 # 127.0.0.1:800/courses/scala = null
+
+# QUERY
+@app.get("/courses/")
+async def get_category_by_query(category: str):
+    courses_to_return = []
+    for course in courses_db:
+        if course.get('category').casefold() == category.casefold():
+            courses_to_return.append(course)
+    return courses_to_return
+
+
+# try on browser: http://127.0.0.1:8000/courses/?category=Development
+
+# PATH AND QUERY
+@app.get("/courses/{course_instructor}/")
+async def get_instructor_category_by_query(course_instructor: str, category: str):
+    courses_to_return = []
+    for course in courses_db:
+        if (course.get('instructor').casefold() == course_instructor.casefold()
+                and course.get('category').casefold() == category.casefold()):
+            courses_to_return.append(course)
+    return courses_to_return
+
+
+# try on web: http://127.0.0.1:8000/courses/burak/?category=development
+# try on web: http://127.0.0.1:8000/courses/burak/?category=ai
+# try on web: http://127.0.0.1:8000/courses/burak/?category=devops
+
+# POST
+@app.post("/courses/create_course")
+async def create_course(new_course=Body()):
+    courses_db.append(new_course)
+
+
+# step 1: open on web -> http://127.0.0.1:8000/docs
+# step 2: expand /courses/create_courses
+# step 3: click "Try it out"
+# step 4: write below of Request body: {'id': 7, 'instructor': 'Ayse', 'title': 'Docker', 'category': 'DevOps'}
+# step 5: click "Execute" button
+# !: Propably you took this error: "422 Error: Unprocessable Entity"
+# step 6: change ' with " (write: {"id": 7, "instructor": "Ayse", "title": "Docker", "category": "DevOps"})
+# step 7: expand /courses Get All Courses
+# step 8: click "Try it out"
+# step 9: click "Execute" button
+
+# UPDATE with PUT
+@app.put("/courses/update_course")
+async def update_course(updated_course=Body()):
+    for index in range(len(courses_db)): # this time, we used index to find correct element
+        if courses_db[index].get("id") == updated_course.get("id"):
+            courses_db[index] = updated_course
+
+
+# step 1: open on web -> http://127.0.0.1:8000/docs
+# step 2: expand /courses/update_course
+# step 3: click "Try it out"
+# step 4: write below of Request body: {'id': 5, 'instructor': 'Cem', 'title': 'ML', 'category': 'AI'}
+# step 5: click "Execute" button
+# !: Propably you took this error: "422 Error: Unprocessable Entity"
+# step 6: change ' with " (write: {"id": 5, "instructor": "Cem", "title": "ML", "category": "AI"})
+# step 7: expand /courses Get All Courses
+# step 8: click "Try it out"
+# step 9: click "Execute" button
+
+# DELETE using PATH
+@app.delete("/courses/delete_course/{course_id}")
+async def delete_course(course_id: int):  # we are using id to delete specific item
+    for index in range(len(courses_db)):
+        if courses_db[index].get("id") == course_id:
+            courses_db.pop(index)
+            break  # we dont need to continue for loop anymore if we found the item
+
+
+# step 1: open on web -> http://127.0.0.1:8000/docs
+# step 2: expand /courses/delete_course
+# step 3: click "Try it out"
+# step 4: write beside of course_id: 2
+# step 5: click "Execute" button
+# step 6: expand /courses Get All Courses
+# step 7: click "Try it out"
+# step 8: click "Execute" button
